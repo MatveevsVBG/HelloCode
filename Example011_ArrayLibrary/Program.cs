6904248ea5e92a3981e045c92e64795f6a748436
Sergei Matveev <msergne@gmail.com>
2023-01-15 21:52:51 +0300

    Add Example011

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;     // если задано значение для поиска вне значений массива, то -1 будет указывать, что элемент не найден.
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;       // после нахождения первого совпадения - выход.
+        }
+        index++;    
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();  //вывод пустой строки
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
