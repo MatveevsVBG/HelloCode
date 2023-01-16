5c12a4b81dab8d8fe850e807bd29a36f5c2b6d3b
Sergei Matveev <msergne@gmail.com>
2023-01-16 19:20:09 +0300

    Added comments

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,47 +1,51 @@
-﻿void FillArray(int[] collection)
+﻿Console.Write("Введите диапазон значений: ");
+int range = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] collection)     // Объявление функции; collection - имя массива в функции
 {
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
+    int length = collection.Length;  // Присваеваем размер массива переменной length
+    int index = 0;                    // Начальный индекс
+    while (index < length)          // Пока текущий индекс значения массива меньше длины массива
     {
-        collection[index] = new Random().Next(1, 10);
+        collection[index] = new Random().Next(1, range);  // Присваивание значения от 1 до 9 с текущим индексом 
         index++;
     }
 }
-
 void PrintArray(int[] col)
 {
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
+    int count = col.Length;      // Присваеваем размер массива переменной count 
+    int position = 0;         // Начальный индекс
+    while (position < count)     // Пока текущий индекс значения массива меньше длины массива
     {
-        Console.Write(col[position]);
+        Console.Write($"{col[position]} ");    //  Вывод в консоль значения с текущим индексом
         position++;
     }
 }
-
-int IndexOf(int[] collection, int find)
+int IndexOf(int[] collection, int find)   // Объявление функции, возвращающей значение; 2 аргумента: массив и переменная find
 {
-    int count = collection.Length;
+    int count = collection.Length;        // Присваеваем размер массива переменной count
     int index = 0;
     int position = -1;     // если задано значение для поиска вне значений массива, то -1 будет указывать, что элемент не найден.
     while (index < count)
     {
         if (collection[index] == find)
         {
-            position = index;
-            break;       // после нахождения первого совпадения - выход.
+            position = index;     //  Если значение найдено, то сохраняем индекс значения в переменную position
+            break;              // после нахождения первого совпадения - выход.
         }
-        index++;    
+        index++;
     }
     return position;
 }
+Console.Write("Введите размер массива: ");
+int len = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[len];    // Задаём размер массива
 
-int[] array = new int[10];
+Console.Write("Введите искомое число: ");
+int nums = Convert.ToInt32(Console.ReadLine());
 
-FillArray(array);
-PrintArray(array);
-Console.WriteLine();  //вывод пустой строки
-
-int pos = IndexOf(array, 444);
-Console.WriteLine(pos);
+FillArray(array);            // Заполняем массив
+PrintArray(array);           // Выводим в консоль массив
+Console.WriteLine();      //вывод пустой строки
+int pos = IndexOf(array, nums);  // Вызов функции поиска и запись результата в переменную pos
+Console.WriteLine($"Позиция: {pos}");          // Вывод в консоль
