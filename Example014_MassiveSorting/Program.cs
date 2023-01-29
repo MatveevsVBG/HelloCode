d220fca05d7de722db4bf7f8c484a25850fb991b
Sergei Matveev <msergne@gmail.com>
2023-01-29 21:22:56 +0300

    Add lection 4

--- Example014_MassiveSorting/Program.cs
+++ Example014_MassiveSorting/Program.cs
@@ -1,37 +1,4 @@
-﻿// Сортировка массива методом перебора.
-
-// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
-
-// void PrintArray(int[] array)
-// {
-//     int count = array.Length;
-
-//     for (int i = 0; i < count; i++)
-//     {
-//         Console.Write($"{array[i]} ");
-//     }
-//     Console.WriteLine();
-// }
-
-// void SelectoinSort(int[] array)
-// {
-//     for (int i = 0; i <= array.Length - 1; i++)
-//     {
-//         int minPosition = i;
-//         for (int j = i + 1; j < array.Length; j++)
-//         {
-//             if(array[j] < array[minPosition]) minPosition = j;
-//         }
-//         int temporary = array[i];
-//         array[i] = array[minPosition];
-//         array[minPosition] = temporary;
-//     }
-// }
-
-// PrintArray(arr);
-// SelectoinSort(arr);
-// PrintArray(arr);
-
+﻿// Сортировка массива методом перебора мин->макс.
 
 int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
@@ -53,7 +20,7 @@ void SelectoinSort(int[] array)
         int minPosition = i;
         for (int j = i + 1; j < array.Length; j++)
         {
-            if(array[j] > array[minPosition]) minPosition = j;
+            if(array[j] < array[minPosition]) minPosition = j;   // Для сортировки max -> min поменять знак с < на > .
         }
         int temporary = array[i];
         array[i] = array[minPosition];
